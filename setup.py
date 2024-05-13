fa36cd583148786e37c7378853bde79477d82bf3
Henrique Peixoto Machado <henrique_machado_hvar@whirlpool.com>
2024-05-13 17:17:56 -0300

    upgrade dist

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='functions.gcexplain',
     packages=find_packages(include=['functions.gcexplain']),
-    version='0.1.0',
+    version='0.1.1',
     description='Library for using Granger causality for Feature Importance Estimation',
     author='Henrique Peixoto Machado',
     install_requires=['tensorflow==2.15.0', 'scikit-learn==1.2.2', 'pandas==2.0.3', 'numpy==1.25.2'],
